3133662b1a5f2956368f387ab54b7604b9a0acb3
Ticiana <ticipp@gmail.com>
2018-05-01 21:19:02 +1000

    Add Hot Module Reloading

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import http from "http";
 import { createServer } from "http";
-import app from "./server";
 
-const server = http.createServer(app);
+import app from "./server";
+let currentApp = app;
+
+if (module.hot) {
+  module.hot.accept(["./server"], () => {
+    server.removeListener("request", currentApp);
+    server.on("request", app);
+    currentApp = app;
+  });
+}
