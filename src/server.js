ce20720e0ef6a5e041ccdf96e91f7fe4b5bd6c9b
Ticiana <ticipp@gmail.com>
2018-05-01 22:00:42 +1000

    Add index to the routers

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ import express from "express";
 
 const app = express();
 
-//Declare a separate router
-const apiRouter = express.Router();
-apiRouter.get("/", (req, res) => res.json({ api: true }));
-apiRouter.all("*", (req, res) => res.json({ apiAll: true }));
+// //Declare a separate router
+// const apiRouter = express.Router();
+// apiRouter.get("/", (req, res) => res.json({ api: true }));
+// apiRouter.all("*", (req, res) => res.json({ apiAll: true }));
 
-//Mount new router to app
-app.use("/api", apiRouter);
+// //Mount new router to app
+// app.use("/api", apiRouter);
 
 app.get("/", (req, res) => res.json({ first: true }));
 app.all("*", (req, res) => {
